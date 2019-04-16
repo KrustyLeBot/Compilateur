a8b098ad8f8ec0ac89c223ab5fbdf565610eee26
M. Wowk Timothee <wowk@insa-07575>
2019-04-16 15:05:07 +0200

    banane

--- test.c
+++ test.c
@@ -4,4 +4,4 @@ main(){int a = 2;int b = 3;int c = a+ b;}
 main(){int a = 2;int b = 3;int c = a+b;}
 main(){int a = 2;int b = 3;int c = a* 2;}
 
-main(){int a = 2;int b;b = 1+a;}
+main(){int a = 2;int b = 2 ;b = -2*4*a+b;}
